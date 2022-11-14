6195fd9bfd23ca07d060a682a2ea9d7f1337d234
Bill Mårtensson <bill@magictechnology.se>
2022-11-14 12:43:33 +0100

    Gjort saker

--- pia11v10mon/ContentView.swift
+++ pia11v10mon/ContentView.swift
@@ -8,19 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var firstname = "Torsten"
+    
+    @State var number = 0.0
+    
+    @State var starttime = Date()
+    
+    @State var mytimer : Timer?
+    
+    @AppStorage("username") var username = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("frog")
+                .resizable()
+                .frame(width: 100, height: 100)
+            Text("hello")
+            Text("welcome \(firstname)")
+            
+            Text("Antal: \(number)")
+                .font(.title)
+            
+            Button(action: {
+                starttimer()
+            }) {
+                Text("Start")
+            }
+            
+            
+            TextField("Namn", text: $username)
+
+            
         }
         .padding()
+        .onAppear() {
+            
+        }
+    }
+    
+    func starttimer() {
+        if(mytimer != nil)
+        {
+            mytimer!.invalidate()
+        }
+        
+        starttime = Date()
+        
+        mytimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            //number = number + 0.1
+            let timepassed = Date().timeIntervalSince(starttime)
+            
+            number = timepassed
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environment(\.locale, .init(identifier: "en"))
     }
 }
